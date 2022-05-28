2d0c7e9e4de46db9c83f48312f789865781d3689
96olbap <alvinwanjala@gmail.com>
2022-05-28 19:31:20 +0300

    This commit will update models.py

--- gallery/models.py
+++ gallery/models.py
@@ -1,24 +1,31 @@
+from tkinter import CASCADE
 from django.db import models
 from django.forms import CharField
 
 # Create your models here.
-class Image(models.Model):
-    image = models.ImageField(upload_to = 'uploads/', default='Wepukhulu')
-    name = models.CharField(max_length=100)
-    description = models.TextField(max_length = 50,blank = True)
-    uploaded_at = models.DateTimeField(auto_now_add=True)
-
-    def __str__(self):
-        return self.name
-
-class Location(models.Model):
-    name = models.CharField(max_length=150)
-
-    def __str__(self):
-        return self.name
 
 class Category(models.Model):
     name = CharField(max_length= 150)
 
     def __str__(self):
         return self.name
+        
+class Location(models.Model):
+    name = models.CharField(max_length=150)
+
+    def __str__(self):
+        return self.name
+
+class Image(models.Model):
+    image = models.ImageField(upload_to = 'uploads/', default='Wepukhulu')
+    name = models.CharField(max_length=100)
+    description = models.TextField(max_length = 50,blank = True)
+    uploaded_at = models.DateTimeField(auto_now_add=True)
+    location = models.ForeignKey(Location, on_delete=models.CASCADE, null=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, null=True)
+
+    def __str__(self):
+        return self.name
+
+    def save_image(self):
+        self.save()
