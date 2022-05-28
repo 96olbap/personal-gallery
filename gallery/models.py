58b8757bca7ad904c27da4569baeeb81d605196e
96olbap <alvinwanjala@gmail.com>
2022-05-28 19:02:42 +0300

    This commit will create the Location and Category models.

--- gallery/models.py
+++ gallery/models.py
@@ -1,8 +1,24 @@
 from django.db import models
+from django.forms import CharField
 
 # Create your models here.
 class Image(models.Model):
-    image = models.ImageField(upload_to = 'uuploads/')
+    image = models.ImageField(upload_to = 'uploads/', default='Wepukhulu')
     name = models.CharField(max_length=100)
     description = models.TextField(max_length = 50,blank = True)
     uploaded_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+
+class Location(models.Model):
+    name = models.CharField(max_length=150)
+
+    def __str__(self):
+        return self.name
+
+class Category(models.Model):
+    name = CharField(max_length= 150)
+
+    def __str__(self):
+        return self.name
