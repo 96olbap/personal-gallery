bbb80573bcebbcd425eb927b899ecfb546a73ec4
96olbap <alvinwanjala@gmail.com>
2022-05-28 18:31:16 +0300

    This commit will install pillow and update models.py to add the image model

--- gallery/models.py
+++ gallery/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Image(models.Model):
+    image = models.ImageField(upload_to = 'uuploads/')
+    name = models.CharField(max_length=100)
+    description = models.TextField(max_length = 50,blank = True)
+    uploaded_at = models.DateTimeField(auto_now_add=True)
