db0505dce9d67d5c0bb98c7f44b924b48b7e592a
96olbap <alvinwanjala@gmail.com>
2022-05-31 03:09:54 +0300

    This commit will update home template styling

--- static/scripts/main.js
+++ static/scripts/main.js
@@ -19,7 +19,7 @@ function myFunction() {
 var modal = document.getElementById("myModal");
 
 // Get the button that opens the modal
-var btn = document.getElementById("myBtn");
+const btn = document.getElementById("myBtn");
 
 // Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
